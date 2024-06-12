b95f199c2bdde775fd4aec6236fc3cb4fac2ed18
ShubhamGaykhe <146503447+ShubhamGaykhe@users.noreply.github.com>
2024-06-12 11:24:13 +0530

    Delete Project/HotelManagement directory

--- Project/HotelManagement/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Project.HotelManagement.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Project.HotelManagement.entities.*;
-
-public interface PaymentRepository extends JpaRepository<Payment,Integer>{
-
-}
